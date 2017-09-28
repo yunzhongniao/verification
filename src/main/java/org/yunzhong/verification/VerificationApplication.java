875eb8d380f5fbfbadba59ff6008f43e93fb76cf
wangyunzhong <wangyunzhong@rongcapital.cn>
2017-09-28 17:11:09 +0800

    init

---
+++ src/main/java/org/yunzhong/verification/VerificationApplication.java
@@ -0,0 +1,12 @@
+package org.yunzhong.verification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerificationApplication.class, args);
+	}
+}
---
+++ src/test/java/org/yunzhong/verification/VerificationApplicationTests.java
@@ -0,0 +1,16 @@
+package org.yunzhong.verification;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VerificationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
